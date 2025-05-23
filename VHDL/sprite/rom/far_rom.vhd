e849afc489601e9fe948acf109f8739d39b0f964
Christian Motta <chrismotta2005@gmail.com>
2025-05-24 09:37:01 +1200

    Finished parallax rendering

--- VHDL/sprite/rom/far_rom.vhd
+++ VHDL/sprite/rom/far_rom.vhd
@@ -42,7 +42,7 @@ USE altera_mf.altera_mf_components.all;
 ENTITY far_rom IS
 	PORT
 	(
-		address		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		address		: IN STD_LOGIC_VECTOR (13 DOWNTO 0);
 		clock		: IN STD_LOGIC  := '1';
 		q		: OUT STD_LOGIC_VECTOR (11 DOWNTO 0)
 	);
@@ -61,15 +61,15 @@ BEGIN
 		address_aclr_a => "NONE",
 		clock_enable_input_a => "BYPASS",
 		clock_enable_output_a => "BYPASS",
-		init_file => "./mifs/far.mif",
+		init_file => "./mifs/farscaled.mif",
 		intended_device_family => "Cyclone V",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
-		numwords_a => 65536,
+		numwords_a => 12288,
 		operation_mode => "ROM",
 		outdata_aclr_a => "NONE",
 		outdata_reg_a => "CLOCK0",
-		widthad_a => 16,
+		widthad_a => 14,
 		width_a => 12,
 		width_byteena_a => 1
 	)
@@ -103,36 +103,36 @@ END SYN;
 -- Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 -- Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
--- Retrieval info: PRIVATE: MIFfilename STRING "./mifs/far.mif"
--- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "65536"
+-- Retrieval info: PRIVATE: MIFfilename STRING "../mifs/farscaled.mif"
+-- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "12288"
 -- Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 -- Retrieval info: PRIVATE: RegAddr NUMERIC "1"
 -- Retrieval info: PRIVATE: RegOutput NUMERIC "1"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 -- Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 -- Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
--- Retrieval info: PRIVATE: WidthAddr NUMERIC "16"
+-- Retrieval info: PRIVATE: WidthAddr NUMERIC "14"
 -- Retrieval info: PRIVATE: WidthData NUMERIC "12"
 -- Retrieval info: PRIVATE: rden NUMERIC "0"
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 -- Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
--- Retrieval info: CONSTANT: INIT_FILE STRING "./mifs/far.mif"
+-- Retrieval info: CONSTANT: INIT_FILE STRING "../mifs/farscaled.mif"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone V"
 -- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
--- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "65536"
+-- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "12288"
 -- Retrieval info: CONSTANT: OPERATION_MODE STRING "ROM"
 -- Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: OUTDATA_REG_A STRING "CLOCK0"
--- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "16"
+-- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "14"
 -- Retrieval info: CONSTANT: WIDTH_A NUMERIC "12"
 -- Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
--- Retrieval info: USED_PORT: address 0 0 16 0 INPUT NODEFVAL "address[15..0]"
+-- Retrieval info: USED_PORT: address 0 0 14 0 INPUT NODEFVAL "address[13..0]"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
 -- Retrieval info: USED_PORT: q 0 0 12 0 OUTPUT NODEFVAL "q[11..0]"
--- Retrieval info: CONNECT: @address_a 0 0 16 0 address 0 0 16 0
+-- Retrieval info: CONNECT: @address_a 0 0 14 0 address 0 0 14 0
 -- Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
 -- Retrieval info: CONNECT: q 0 0 12 0 @q_a 0 0 12 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL far_rom.vhd TRUE
